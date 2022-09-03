2191e0b5c94fef3297fd8cfd35fb2c416d985c63
pan-alex <alexanderypan@gmail.com>
2022-09-02 20:08:51 -0400

    clean-up: remove console.log

--- routes/search.js
+++ routes/search.js
@@ -3,8 +3,6 @@ const router = express.Router()
 const searchController = require("../controllers/search");
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
-console.log('Inside search routes')
-
 router.get('/', ensureAuth, searchController.getMovie)
 
 module.exports = router;
