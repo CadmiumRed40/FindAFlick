a0f9d16e5509a689dc00cba1e01c1d04c4617250
Alliegie <100817906+AllieGie@users.noreply.github.com>
2022-09-05 02:00:38 -0400

    upated first and last name validation errors

--- controllers/auth.js
+++ controllers/auth.js
@@ -55,23 +55,32 @@ const User = require('../models/User')
   }
   
   exports.postSignup = (req, res, next) => {
-    const validationErrors = []
-    if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
-    if (!validator.isLength(req.body.password, { min: 8 })) validationErrors.push({ msg: 'Password must be at least 8 characters long' })
-    if (req.body.password !== req.body.confirmPassword) validationErrors.push({ msg: 'Passwords do not match' })
-    if (!validator.isText(req.body.firstName)) validationErrors.push({ msg: 'Please enter your Name.' }) 
-    if (!validator.isText(req.body.lastName)) validationErrors.push({ msg: 'Please enter last Name.' })
-  
+    const validationErrors = [];
+    if (!validator.isEmail(req.body.email))
+        validationErrors.push({ msg: "Please enter a valid email address." });
+    if (!validator.isLength(req.body.password, { min: 8 }))
+        validationErrors.push({
+            msg: "Password must be at least 8 characters long",
+        });
+    if (req.body.password !== req.body.confirmPassword)
+        validationErrors.push({ msg: "Passwords do not match" });  
+    if (req.body.firstName !== req.body.firstName)
+      validationErrors.push({ msg: 'Please enter your Name.' }) 
+    if (req.body.lastName !== req.body.lastName)
+      validationErrors.push({ msg: 'Please enter your last Name.' }) 
     if (validationErrors.length) {
-      req.flash('errors', validationErrors)
-      return res.redirect('../signup')
+        req.flash("errors", validationErrors);
+        return res.redirect("../signup");
     }
+    
     req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
   
     const user = new User({
       userName: req.body.userName,
       email: req.body.email,
-      password: req.body.password
+      password: req.body.password,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName
     })
   
     User.findOne({$or: [
