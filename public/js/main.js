5b849bf7e84c274d49be9835c5a3998d4dc8fb3a
pan-alex <alexanderypan@gmail.com>
2022-09-05 23:29:20 -0400

    Feature: Add alert if no match found in database

--- public/js/main.js
+++ public/js/main.js
@@ -76,7 +76,6 @@ async function getMovie() {
         const response = await fetch(`search/?title=${givenTitle}`);
         const data = await response.json().then(x => x);
         addMovieDataToDOM(data[0]);
-
     } catch (err) {
         console.log(err)
     }
@@ -85,18 +84,18 @@ async function getMovie() {
 
 
 async function addMovieDataToDOM(data) {
-    document.querySelector('.movieInfo').classList.remove('displayNone');
-    document.querySelector('#moviePoster').src = data.poster
-    document.querySelector('#movieTitle').innerText = data.title
-    document.querySelector('#movieYear').innerText = data.year
-    document.querySelector('#movieDirectors').innerText = data.directors.join(', ')
-    document.querySelector('#movieWriters').innerText = data.writers.join(', ')
-    document.querySelector('#movieLeadActors').innerText = data.cast.join(', ')
-    document.querySelector('#movieIMDB').innerText = data.imdb.rating
-    document.querySelector('#movieTomatoes').innerText = data.tomatoes.viewer.rating
-    document.querySelector('#movieSynopsis').innerText = data.fullplot
-
-
-
-
+    if (data) {
+        document.querySelector('.movieInfo').classList.remove('displayNone');
+        document.querySelector('#moviePoster').src = data.poster
+        document.querySelector('#movieTitle').innerText = data.title
+        document.querySelector('#movieYear').innerText = data.year
+        document.querySelector('#movieDirectors').innerText = data.directors.join(', ')
+        document.querySelector('#movieWriters').innerText = data.writers.join(', ')
+        document.querySelector('#movieLeadActors').innerText = data.cast.join(', ')
+        document.querySelector('#movieIMDB').innerText = data.imdb.rating
+        document.querySelector('#movieTomatoes').innerText = data.tomatoes.viewer.rating
+        document.querySelector('#movieSynopsis').innerText = data.fullplot
+    } else {
+        alert('Media not found. Please try another movie.');
+    }
 }
