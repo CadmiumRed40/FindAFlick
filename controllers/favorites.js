a755fa891a2757a98ddfa815f62d5cdf78941da8
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-02-15 20:28:06 -0500

    adding a favorites controller

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -1,1 +1,20 @@
-const Favorites = require('..models/favorites')
+const Favorites = require('..models/flix')
+
+module.exports = {
+    getFlix: async (req, res) => {
+        console.log(req.user)
+        try {
+            const flixFavorites = await Flix.find({
+                userId:req.user.id,
+            completes: true,
+        });
+        res.render("favorites.ejs", {
+            flix: flixFavorites,
+            user: req.user,
+        });
+        }catch(err){
+            console.log(err)
+        }
+    },
+
+}
